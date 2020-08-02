88745253096bfa99dead374f8e3ae1ad31763d94
grtlinux <grtlinux@gmail.com>
2020-08-02 19:36:00 +0900

    create 1st

---
+++ ver-05.01/KieaJpaRelation-05-OneToOne-01/src/main/java/org/tain/KieaJpaRelation05OneToOne01Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaJpaRelation05OneToOne01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaJpaRelation05OneToOne01Application.class, args);
+	}
+
+}
