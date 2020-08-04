dd2a7fa178257c7505a48dd71e8bdbc5b478f097
grtlinux <grtlinux@gmail.com>
2020-08-04 17:00:07 +0900

    add json browser

--- ver-05.01/KieaJpaRelation-05-OneToOne-01/src/main/java/org/tain/config/WebMvcConfig.java
+++ ver-05.01/KieaJpaRelation-05-OneToOne-01/src/main/java/org/tain/config/WebMvcConfig.java
@@ -1,22 +1,29 @@
 package org.tain.config;
 
-//@Configuration
-//@EnableWebMvc
-public class WebMvcConfig {
-//public class WebMvcConfig extends WebMvcConfigurerAdapter {
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	/*
 	 * Browser JSON Pretty Printing
 	 */
 	
-	/*
-	@Bean
 	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
 		MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
 		mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper());
 		return mappingJackson2HttpMessageConverter;
 	}
 
-	@Bean
 	public ObjectMapper objectMapper() {
 		ObjectMapper objMapper = new ObjectMapper();
 		objMapper.enable(SerializationFeature.INDENT_OUTPUT);
@@ -25,8 +32,7 @@ public class WebMvcConfig {
 
 	@Override
 	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-		super.configureMessageConverters(converters);
+		//super.configureMessageConverters(converters);
 		converters.add(mappingJackson2HttpMessageConverter());
 	}
-	*/
 }
